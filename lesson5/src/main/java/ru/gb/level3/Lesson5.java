16cc6473e21d74df36362a644843b83f04b23824
vladkomarov <vlkomdev-gb@mail.ru>
2022-09-11 21:51:03 +0300

    lesson5

---
+++ lesson5/src/main/java/ru/gb/level3/Lesson5.java
@@ -0,0 +1,27 @@
+package ru.gb.level3;
+
+public class Lesson5 {
+
+    public static final int CARS_COUNT = 4;
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        }
+
+        for (Car car : cars) {
+            new Thread(car).start();
+        }
+
+        race.readyAwait();
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+
+        race.finishAwait();
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+}
